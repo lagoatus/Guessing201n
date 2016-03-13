a5e9f5b8eb4ef70b358304621c8f796d6439b960
Amina Senge <aminasenge5@gmail.com>
2016-03-13 11:45:05 -0700

    changed questions to Yes answers

--- Guessing.js
+++ Guessing.js
@@ -2,25 +2,27 @@
 
 var welcome=prompt("Welcome to my page!! Please answer a few questions that will help you get to know me...Click OK to continue...")
 
-var greeting;
 
 //These are the questions about me to the user//
 
-var answer1=prompt("What is my first name?");
+var answer1=prompt("Is my name Amina?");
 
-	if (answer1="Amina")	{
+	if (answer1="Yes")	{
 
 		alert("Yes! My name is Amina")}
 
+//no else statement is included so the alert will pop-up even if the answer is incorrect
 
-var answer2=prompt("How many children do I have?");
+var answer2=prompt("Do I have 5 children?");
 
-	if(answer2="5") {
+	if(answer2="Yes") {
 
 		alert("Yes! I have 5 children!")}
 
-var answer3=prompt("What instrument do I play?");
+var answer3=prompt("Do I play the Trombone?");
 
-	if(answer3="Trombone")	{
+	if(answer3="Yes")	{
 
 			alert("Yes! I play the Trombone!")}
+
+//End of questions.
