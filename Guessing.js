c8daaa442571111d94816bbf0fc66a49e3ae485c
Amina Senge <aminasenge5@gmail.com>
2016-03-13 11:30:04 -0700

    index and js for guessing game

---
+++ Guessing.js
@@ -0,0 +1,26 @@
+//Created a welcome message for the user//
+
+var welcome=prompt("Welcome to my page!! Please answer a few questions that will help you get to know me...Click OK to continue...")
+
+var greeting;
+
+//These are the questions about me to the user//
+
+var answer1=prompt("What is my first name?");
+
+	if (answer1="Amina")	{
+
+		alert("Yes! My name is Amina")}
+
+
+var answer2=prompt("How many children do I have?");
+
+	if(answer2="5") {
+
+		alert("Yes! I have 5 children!")}
+
+var answer3=prompt("What instrument do I play?");
+
+	if(answer3="Trombone")	{
+
+			alert("Yes! I play the Trombone!")}
